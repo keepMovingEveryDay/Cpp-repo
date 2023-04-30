dde2dab81cfab5a5c567d7b6f77d2df4d8169c8d
Samuel Dai <samueldai@Samuels-macbookpro.local>
2023-04-30 19:20:36 +0800

    0 Update print i love you

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
     printf("Hello World\n");
     std::vector<int> v;
     v.push_back(1);
-    std::cout<< v[1];
+    std::cout << "I love you" << std::endl;
+    std::cout << v[1];
     return 0;
 }
