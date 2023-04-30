ceeddb15e5045a60f78438d1263a123ae1238468
Samuel Dai <samueldai@Samuels-macbookpro.local>
2023-04-30 17:21:45 +0800

    0 Update print hello

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+int main()
+{
+    printf("Hello World\n");
+    std::vector<int> v;
+    v.push_back(1);
+    std::cout<< v[1];
+    return 0;
+}
